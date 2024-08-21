2047936ef71e8ac415db2a7e1e0e717eee9bc6a4
KabakiAntony <kabaki.antony@gmail.com>
2024-08-21 09:30:13 +0300

    create urls for the blog app

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from .views import (
+    blog_create, 
+    blog_delete, 
+    blog_detail, 
+    blog_list,
+    blog_update)
+
+app_name = "blog"
+
+urlpatterns = [
+    path('', blog_list, name='blog_list'),
+    path('create/', blog_create, name='blog_create'),
+    path('delete/<int:id>', blog_delete, name='blog_delete'),
+    path('read/<int:id', blog_detail, name='blog_detail'),
+    path('update/<int:id', blog_update, name='blog_update'),
+]
